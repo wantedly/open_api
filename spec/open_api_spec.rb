ee7088185a32e5cbd548def55e87410d6e016125
Kent Nagata <ngtknt@me.com>
2018-05-01 17:28:42 +0900

    Remove unuseful example

--- spec/open_api_spec.rb
+++ spec/open_api_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe OpenApi do
   it "has a version number" do
     expect(OpenApi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
