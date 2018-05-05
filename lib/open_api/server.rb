1f082e5d84710084a8f3670c797267bd8085d622
Kent Nagata <ngtknt@me.com>
2018-05-05 20:46:11 +0900

    Add Server.load

--- lib/open_api/server.rb
+++ lib/open_api/server.rb
@@ -1,6 +1,8 @@
 module OpenApi
   # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#server-object
   class Server
+    prepend EquatableAsContent
+
     attr_accessor :url, :description, :variables
 
     def initialize(url:, description: nil, variables: nil)
--- spec/open_api/server_spec.rb
+++ spec/open_api/server_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe OpenApi::Server do
       )
     ).to be_a(described_class)
   end
+
+  describe ".load" do
+    subject { described_class.load(hash) }
+
+    let(:hash) do
+      {
+        "url" => "https://example.com",
+        "description" => "Desc",
+        "variables" => [variable_hash],
+      }
+    end
+    let(:variable_hash) { double(:variable_hash) }
+    let(:variable) { double(:variable) }
+
+    before do
+      allow(OpenApi::ServerVariable).to receive(:load).with(variable_hash).and_return(variable)
+    end
+
+    it "creates an instance" do
+      is_expected.to eq described_class.new(
+        url: "https://example.com",
+        description: "Desc",
+        variables: [variable],
+      )
+    end
+  end
 end
