1bd4a598b2af92ca9283f6af2c401292e5fe3d89
Kent Nagata <ngtknt@me.com>
2018-05-06 00:20:29 +0900

    Add Header.load

--- lib/open_api/header.rb
+++ lib/open_api/header.rb
@@ -1,6 +1,8 @@
 module OpenApi
   # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#header-object
   class Header
+    prepend EquatableAsContent
+
     attr_accessor :description, :required, :deprecated, :allow_empty_value
 
     def initialize(description: nil, required: false, deprecated: nil, allow_empty_value: false, **other_fields_hash)
@@ -18,6 +20,20 @@ module OpenApi
           other_fields_hash[field_name] = value
         end
       end
+    end
+
+    def self.load(hash)
+      other_fields_hash = hash.reject { |key|
+        key.to_sym.in?([:description, :required, :deprecated, :allowEmptyValue])
+      }.symbolize_keys
+
+      new(
+        description: hash["description"],
+        required: (hash["required"].nil? ? false : hash["required"]),
+        deprecated: (hash["deprecated"].nil? ? false : hash["deprecated"]),
+        allow_empty_value: (hash["allowEmptyValue"].nil? ? false : hash["allowEmptyValue"]),
+        **other_fields_hash,
+      )
     end
 
     private
