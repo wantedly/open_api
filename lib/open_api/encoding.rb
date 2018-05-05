dabc4ca2ae9b47a6e07edadb87c1e6eb39654bd5
Kent Nagata <ngtknt@me.com>
2018-05-06 01:00:19 +0900

    Add Encoding.load

--- lib/open_api/encoding.rb
+++ lib/open_api/encoding.rb
@@ -1,6 +1,8 @@
 module OpenApi
+  # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#encodingObject
   class Encoding
-    # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#encodingObject
+    prepend EquatableAsContent
+
     attr_accessor :content_type, :headers, :style, :explode, :allow_reserved
 
     def initialize(content_type: nil, headers: nil, style: nil, explode: nil, allow_reserved: false)
@@ -14,7 +16,7 @@ module OpenApi
     def self.load(hash)
       new(
         content_type: hash["contentType"]&.to_s,
-        headers: hash["headers"]&.map { |k, v| [k, Reference.laod(v) || Header.load(v)] }.to_h,
+        headers: hash["headers"]&.map { |k, v| [k, Reference.load(v) || Header.load(v)] }.to_h,
         style: hash["style"]&.to_s,
         explode: hash["explode"],
         allow_reserved: hash["allowReserved"].nil? ? false : hash["allowReserved"],
