aaae963ff537ad127f841545ec25c8175d4d64e5
Kent Nagata <ngtknt@me.com>
2018-05-06 02:26:04 +0900

    Follow nil case

--- lib/open_api/path_item.rb
+++ lib/open_api/path_item.rb
@@ -19,11 +19,11 @@ module OpenApi
     def self.load(hash)
       operations = hash.select { |key| key.to_sym.in?(OPERATION_NAMES) }
       new(
-        ref: hash["$ref"].to_s,
-        summary: hash["summary"].to_s,
-        description: hash["description"].to_s,
-        servers: hash["servers"].map { |server_hash| Server.load(server_hash) },
-        parameters: hash["parameters"].map { |h| Reference.load(h) || Parameter.load(h) },
+        ref: hash["$ref"]&.to_s,
+        summary: hash["summary"]&.to_s,
+        description: hash["description"]&.to_s,
+        servers: hash["servers"]&.map { |server_hash| Server.load(server_hash) },
+        parameters: hash["parameters"]&.map { |h| Reference.load(h) || Parameter.load(h) },
         **operations.map { |k ,v| [k.to_sym, Operation.load(v)] }.to_h,
       )
     end
