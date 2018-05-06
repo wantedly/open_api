be25021ced50649a7573ff52a942da0c0cfbe3f4
Kent Nagata <ngtknt@me.com>
2018-05-06 18:20:24 +0900

    Add OAuthFlow.load

--- lib/open_api/o_auth_flow.rb
+++ lib/open_api/o_auth_flow.rb
@@ -1,6 +1,8 @@
 module OpenApi
   # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#oauthFlowObject
   class OAuthFlow
+    prepend EquatableAsContent
+
     attr_accessor :authorization_url, :token_url, :refresh_url ,:scopes
 
     def initialize(authorization_url:, token_url:, refresh_url: nil, scopes:)
@@ -11,6 +13,14 @@ module OpenApi
     end
 
     def self.load(hash)
+      return unless hash
+
+      new(
+        authorization_url: hash["authorizationUrl"],
+        token_url: hash["tokenUrl"],
+        refresh_url: hash["refreshUrl"],
+        scopes: hash["scopes"],
+      )
     end
   end
 end
--- spec/open_api/o_auth_flow_spec.rb
+++ spec/open_api/o_auth_flow_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe OpenApi::OAuthFlow do
       )
     ).to be_a(described_class)
   end
+
+  describe ".load" do
+    subject { described_class.load(hash) }
+
+    let(:hash) do
+      {
+        "authorizationUrl" => "https://example.com",
+        "tokenUrl" => "https://example.com",
+        "scopes" => {
+          "write:pets": "modify pets in your account",
+          "read:pets": "read your pets"
+        }
+      }
+    end
+
+    it "creates an instance from hash" do
+      is_expected.to eq described_class.new(
+        authorization_url: "https://example.com",
+        token_url: "https://example.com",
+        scopes: {
+          "write:pets": "modify pets in your account",
+          "read:pets": "read your pets"
+        },
+      )
+    end
+  end
 end
