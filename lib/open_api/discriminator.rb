5173acdf5f92c511d427602a0dda709469bef3db
Kent Nagata <ngtknt@me.com>
2018-05-06 17:53:51 +0900

    Add Discriminator.load

--- lib/open_api/discriminator.rb
+++ lib/open_api/discriminator.rb
@@ -1,6 +1,8 @@
 module OpenApi
   # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#discriminatorObject
   class Discriminator
+    prepend EquatableAsContent
+
     attr_accessor :property_name, :mapping
 
     def initialize(property_name: nil, mapping: {}, **other_fields_hash)
@@ -16,6 +18,19 @@ module OpenApi
           other_fields_hash[field_name] = value
         end
       end
+    end
+
+    def self.load(hash)
+      return unless hash
+
+      fixed_field_names = %w(propertyName mapping)
+      other_fields_hash = hash.reject { |key| key.to_s.in?(fixed_field_names) }
+
+      new(
+        property_name: hash["propertyName"].to_s,
+        mapping: hash["mapping"],
+        **other_fields_hash.symbolize_keys,
+      )
     end
 
     private
