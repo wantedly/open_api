421dff964630680f36eac3b5ce22d9f7897bbd83
Kent Nagata <ngtknt@me.com>
2018-05-06 19:17:34 +0900

    Add spec for serialization

--- spec/open_api/serializers/yaml_serializer_spec.rb
+++ spec/open_api/serializers/yaml_serializer_spec.rb
@@ -1,4 +1,65 @@
 RSpec.describe OpenApi::Serializers::YamlSerializer do
+  describe "#serialize" do
+    subject { described_class.new.serialize(specification) }
+
+    let(:specification) do
+      OpenApi::Specification.new(
+        openapi: "3.0.1",
+        info: OpenApi::Info.new(
+          title: "Awesome API",
+          description: "It provides something awesome",
+          version: "1.0.0",
+        ),
+        paths: OpenApi::Paths.new(
+          "/pets": OpenApi::PathItem.new(
+            get: OpenApi::Operation.new(
+              description: "Returns all pets from the system that the user has access to",
+              responses: OpenApi::Responses.new(
+                "200": OpenApi::Response.new(
+                  description: "A list of pets.",
+                  content: {
+                    "application/json": OpenApi::MediaType.new(
+                      schema: OpenApi::Schema.new(
+                        type: "array",
+                        items: OpenApi::Reference.new(ref: "#/components/schemas/pet"),
+                      )
+                    )
+                  }
+                )
+              )
+            )
+          )
+        )
+      )
+    end
+
+    it "creates yaml string from specification object" do
+      is_expected.to eq(
+<<-EOL
+---
+openapi: "3.0.1"
+info:
+  title: "Awesome API"
+  description: "It provides something awesome"
+  version: "1.0.0"
+paths:
+  /pets:
+    get:
+      description: "Returns all pets from the system that the user has access to"
+      responses:
+        200:
+          description: "A list of pets."
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: "#/components/schemas/pet"
+EOL
+      )
+    end
+  end
+
   describe "#deserialize" do
     subject { described_class.new.deserialize(yaml_string) }
 
