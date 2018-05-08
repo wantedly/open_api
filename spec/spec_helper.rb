a21287755954982233d8a47e3d865c103130ae75
Kent Nagata <ngtknt@me.com>
2018-05-08 10:21:34 +0900

    Fix order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+require "simplecov"
+SimpleCov.start
+
 require "bundler/setup"
 require "open_api"
 require "pry"
-require "simplecov"
-
-SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
